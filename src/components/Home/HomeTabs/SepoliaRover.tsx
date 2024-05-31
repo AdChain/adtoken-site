39d9409fa0326b82bcc0b9a8848029b5dabafe6e
Matt Zamora <mzamora2899@gmail.com>
2024-05-31 15:25:11 -0600

    style updates

--- src/components/Home/HomeTabs/SepoliaRover.tsx
+++ src/components/Home/HomeTabs/SepoliaRover.tsx
@@ -10,13 +10,13 @@ const SepoliaRover = () => {
         <div className="grid lg:grid-cols-12 ">
           <div className="lg:col-span-2 lg:my-9 my-3">
             <div className="lg:flex justify-center">
-              <p className="text-[#0C8DFF] opacity-80 text-xl tracking-wide">
+              <p className="text-white opacity-80 text-xl tracking-wide">
                 OBJECTIVE
               </p>
             </div>
           </div>
           <div className="lg:col-span-10">
-            <p className="text-[#95A0C5] leading-7 text-sm">
+            <p className="text-white leading-7 text-sm">
               AdChain was deployed to transform the advertising industry by
               combating ad fraud and fostering transparency. Our innovative
               solutions, like the adChain publisher registry and Ads.txt Plus,
@@ -30,7 +30,7 @@ const SepoliaRover = () => {
         <div className="grid lg:grid-cols-12  items-center ">
           <div className="lg:col-span-2 lg:mt-9">
             <div className="lg:flex justify-center items-center">
-              <p className="text-[#0C8DFF] opacity-80 text-xl tracking-wide">
+              <p className="text-white opacity-80 text-xl tracking-wide">
                 PHASES
               </p>
             </div>
@@ -40,23 +40,11 @@ const SepoliaRover = () => {
               <div className="lg:col-span-4 my-2">
                 <p className="text-xl tracking-wide">ADT GENERATION</p>
                 <div className="mt-3">
-                  <svg
-                    width="74"
-                    height="11"
-                    viewBox="0 0 74 11"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>JUNE. 26, 2017</title>
-                    <path
-                      d="M6.64.96c0-.276-.228-.408-.504-.408s-.528.132-.528.408v3.6l.036.648-.408-.516L1.78.888C1.516.6 1.444.552 1.132.552c-.348 0-.48.228-.48.432v7.68c0 .276.252.42.528.42s.504-.144.504-.42V2.88l-.036-.72.36.456 3.6 4.02v2.028c0 .276.252.42.528.42s.504-.144.504-.42V.96zm8.34 1.692v4.356c0 1.14-.912 2.064-2.16 2.064h-1.932c-1.248 0-2.16-.924-2.16-2.064V2.652c0-1.152.96-2.076 2.112-2.076h2.016c1.2 0 2.124.924 2.124 2.076zm-4.188 5.436h2.124c.612 0 1.056-.48 1.056-1.008V2.568c0-.552-.444-1.008-1.008-1.008h-2.22c-.588 0-1.008.444-1.008 1.008V7.08c0 .54.432 1.008 1.056 1.008zM22.3 7.08c.372-.396.54-.744.54-1.452V.96c0-.276-.24-.408-.516-.408-.288 0-.528.132-.528.408v4.608c0 .468-.06.648-.288.9l-1.572 1.668h-.144l-1.596-1.668c-.24-.252-.288-.432-.288-.9V.96c0-.276-.252-.408-.528-.408s-.528.132-.528.408v4.668c0 .708.168 1.056.552 1.452l1.416 1.512c.384.42.564.6 1.032.6.468 0 .636-.18 1.02-.6L22.3 7.08zm2.844 1.968c.48 0 .864-.408.864-.888 0-.48-.384-.888-.864-.888-.468 0-.852.408-.852.888 0 .48.384.888.852.888zm9.18-.972V.648h-2.028c-.276 0-.42.18-.42.456 0 .288.144.48.42.48h1.02v6.492h-1.02c-.276 0-.42.168-.42.444 0 .288.144.48.42.48h3.084c.276 0 .42-.192.42-.48 0-.276-.144-.444-.42-.444h-1.056zm4.032.84c0 .828-.684.756-.684 1.176 0 .084.06.228.24.228.144 0 .288-.06.42-.168.612-.516.9-1.056.9-1.92 0-.48-.36-.888-.876-.888s-.9.3-.9.84c0 .336.228.732.66.732h.24zm7.956-.9v-.108c0-1.788 4.608-2.724 4.608-4.956v-.6c0-.996-.78-1.776-1.896-1.776h-1.968c-1.2 0-1.872.852-1.872 1.944 0 .276.24.432.516.432s.54-.156.54-.432c0-.672.3-.936.864-.936h2.028c.468 0 .744.408.744.792v.552c0 1.56-4.692 2.532-4.692 5.196V9h5.544c.276 0 .42-.204.42-.492 0-.276-.144-.492-.42-.492h-4.416zm12.876-5.328V6.96c0 1.116-.876 2.04-2.028 2.04h-1.848c-1.152 0-2.04-.888-2.04-2.04V2.688c0-1.092.84-2.04 2.04-2.04h1.848c1.212 0 2.028.948 2.028 2.04zm-3.972 5.328h2.04c.516 0 .924-.456.924-.972v-4.44c0-.48-.396-.972-.924-.972h-2.04c-.492 0-.936.444-.936.972v4.44c0 .444.408.972.936.972zm8.604.06V.648h-2.028c-.276 0-.42.18-.42.456 0 .288.144.48.42.48h1.02v6.492h-1.02c-.276 0-.42.168-.42.444 0 .288.144.48.42.48h3.084c.276 0 .42-.192.42-.48 0-.276-.144-.444-.42-.444H63.82zm6.168-1.5c0-1.848 3.084-2.916 3.084-4.848V.648h-5.28c-.276 0-.408.216-.408.492s.132.492.408.492h4.14v.36c0 1.212-3.012 2.46-3.012 4.656v2.016c0 .276.276.42.552.42s.516-.144.516-.42V6.576z"
-                      fill="#0C8DFF"
-                      fillRule="evenodd"
-                    ></path>
-                  </svg>
+                  <p className="text-[#0C8DFF] text-xs">JUNE. 26, 2017</p>
                 </div>
               </div>
               <div className="lg:col-span-7">
-                <p className="text-sm text-[#95A0C5]">
+                <p className="text-sm text-white">
                   AdChain introduced AdToken, a virtuous utility token designed
                   to empower transparency and trust in the digital advertising
                   industry through blockchain technology.
@@ -72,7 +60,7 @@ const SepoliaRover = () => {
                 </div>
               </div>
               <div className="lg:col-span-7">
-                <p className="text-sm text-[#95A0C5]">
+                <p className="text-sm text-white">
                   Experience a new era of transparent digital advertising with
                   adChain Registry on Ethereum Mainnet, ensuring trust and
                   accountability in ad placement. Our work gave birth to the
@@ -89,7 +77,7 @@ const SepoliaRover = () => {
                 </div>
               </div>
               <div className="lg:col-span-7">
-                <p className="text-sm text-[#95A0C5]">
+                <p className="text-sm text-white">
                   Introducing adChain Audits: Your Independent Shield Against Ad
                   Fraud, Ensuring Transparency and Trust in Digital Advertising.
                 </p>
